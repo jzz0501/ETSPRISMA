6cee9e95b3bda3ca0273017da460bd57209bcfc7
ZONGZHEN_PROJECT <jiangzongzhen51@gmail.com>
2023-03-07 09:41:33 +0000

    COMMIT

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+import java.util.Collections;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+        Clase anonima implementar el interface comparator y reescribir su metodo compare, treeset se trata de un estructura de datos red black tree,
+        se va a comparar desde el enlace de raiz, si el resultado de comparacion es negativo se va a comparar o guardar al subenlace de izquierda,
+        si es positivo pues se guardar o comparar en el subenlace derecha, y tiene una serie de regla para hacer el equilibrio del arbol, mediante esta algoritmo
+        por defecto se ordena desde menor a mayor con codigo de la tabla ascii, y podemos utilizar el interface comparator o comparable con su metodo
+        reescribido para cambiar su regla de ordenacion.
+        */
+
+        /*
+        TreeSet<Prisma> ts = new TreeSet<>(new Comparator<Prisma>() {
+            @Override
+            public int compare(Prisma o1, Prisma o2) {
+                return o1.areaTotal() > o2.areaTotal()? -1 : 1;
+            }
+        });
+        */
+
+        /*
+        Simplificado con lambda expression: puede quitar el nombre del metodo, el tipo de dato en dentro de argumento, comentario de reescribir,
+        poner el simbolo de expresion lambda -> para expresar relacion entre argumento y instruccion.
+        */
+
+        TreeSet<Prisma> ts = new TreeSet<>((Prisma o1, Prisma o2) -> o1.areaTotal() > o2.areaTotal()? -1 : 1);
+
+        //metodo de generar prisma
+
+        setPrisma(ts);
+
+        //metodo de imprimir resultado
+
+        printResult(ts);
+
+    }
+
+    /**
+     * metodo para generar prisma con valor aleatoria y se guarda al treeset
+     * @param ts lista de treeset
+     */
+
+    public static void setPrisma(TreeSet<Prisma> ts){
+
+        Prisma prisma1 = new Prisma((float)(Math.random()*100+1),(float)(Math.random()*100+1));
+        Prisma prisma2 = new Prisma((float)(Math.random()*100+1),(float)(Math.random()*100+1));
+        Prisma prisma3 = new Prisma((float)(Math.random()*100+1),(float)(Math.random()*100+1));
+
+        Collections.addAll(ts,prisma1,prisma2,prisma3);
+
+    }
+
+    /**
+     * metodo para imprimir resultado
+     * @param ts lista de treeset
+     */
+
+    public static void printResult(TreeSet<Prisma> ts){
+        ts.forEach(prisma -> System.out.println(prisma+"\n"+"--------------------------------------"));
     }
 }
--- src/Prisma.java
+++ src/Prisma.java
@@ -1,2 +1,117 @@
-package PACKAGE_NAME;public class Prisma {
+public class Prisma {
+
+    /**
+     * Propiedades de la prisma que fue introducido por el usuario
+     */
+    float baseTriangulo;
+    float alturaPrisma;
+
+    /**
+     * Constructor sin parametro para inicializar el objeto
+     */
+
+    public Prisma() {
+    }
+
+    /**
+     * Constructor con parametro
+     * @param baseTriangulo base triangulo que fue introducido por usuario
+     * @param alturaPrisma altura prisma que fue introducido por usuario
+     */
+
+    public Prisma(float baseTriangulo, float alturaPrisma) {
+        this.baseTriangulo = baseTriangulo;
+        this.alturaPrisma = alturaPrisma;
+    }
+
+    /**
+     * Obtener el base de triangulo
+     * @return devolver el valor del base triangulo
+     */
+
+    public float getBaseTriangulo() {
+        return baseTriangulo;
+    }
+
+    /**
+     * Poner el base de triangulo
+     * @param baseTriangulo poner el valor del base triangulo
+     */
+
+    public void setBaseTriangulo(float baseTriangulo) {
+        this.baseTriangulo = baseTriangulo;
+    }
+
+    /**
+     * Obtener la altura de la prisma
+     * @return devolver el valor de la altura prisma
+     */
+
+    public float getAlturaPrisma() {
+        return alturaPrisma;
+    }
+
+    /**
+     * Poner la altura de la prisma
+     * @param alturaPrisma poner la altura prisma
+     */
+
+    public void setAlturaPrisma(float alturaPrisma) {
+        this.alturaPrisma = alturaPrisma;
+    }
+
+    /**
+     * Obtener la altura de triangulo
+     * @return devolver la altura triangulo
+     */
+
+    public float alturaTriangulo() {
+        return ((float)Math.sqrt(3))*baseTriangulo/2f;
+    }
+
+    /**
+     * Obtener la area de base
+     * @return devolver el valor de la area base
+     */
+
+    public float areaBase() {
+        return (baseTriangulo*alturaTriangulo())/2f;
+    }
+
+    /**
+     * Obtener el perimetro
+     * @return devolver el valor del perimetro
+     */
+
+    public float perimetro() {
+        return baseTriangulo*3;
+    }
+
+    /**
+     * Obtener la area lateral
+     * @return devolver el valor de la area lateral
+     */
+
+    public float areaLateral() {
+        return perimetro()*alturaPrisma;
+    }
+
+    /**
+     * Obtener la area total
+     * @return devolver el valor de la area total
+     */
+
+    public float areaTotal() {
+        return (2f*areaBase())+areaLateral();
+    }
+
+    /**
+     * Obtener una string con informacion de la prisma calculado
+     * @return devolver una string con la informacion de la prisma despues de calculado
+     */
+
+    @Override
+    public String toString() {
+        return "Base: " + baseTriangulo + '\n' + "Altura: " + alturaPrisma + '\n' + "Area total: " + areaTotal();
+    }
 }
